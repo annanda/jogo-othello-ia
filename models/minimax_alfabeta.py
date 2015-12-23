d4ad56225881b8de264db5e7b1693d3eb18c57d4
Annanda Sousa <annanda.sousa@gmail.com>
2015-12-23 01:51:21 -0200

    Arrumando uns detalhes em mini_max

--- models/minimax_alfabeta.py
+++ models/minimax_alfabeta.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
+from models.move import Move
 
 class MiniMaxAlfaBeta(object):
 
     def __init__(self):
-        self.chosen_move
-        pass
+        self.chosen_move = None
 
-
-    def mini_max_alfa_beta(board, depth, color, parent_alfa, parent_beta, max_gamer, heuristic_function):
+    def mini_max_alfa_beta(self, board, depth, color, parent_alfa, parent_beta, max_gamer, heuristic_function):
         """
         """
-
         if(depth == 0):
             heuristic_value = heuristic_function(board, color)
             return heuristic_value
+
 
         alfa = float('-inf')
         beta = float('inf')
@@ -23,12 +22,12 @@ class MiniMaxAlfaBeta(object):
             board_copy = board.get_clone()
             move = Move(valid_move)
             board_copy.play(move, color)
-            best_value = mini_max_alfa_beta(
+            best_value = self.mini_max_alfa_beta(
                 board_copy,
+                depth - 1,
                 enemy_color,
                 alfa,
                 beta,
-                depth - 1,
                 not(max_gamer),
                 heuristic_function
             )
@@ -42,6 +41,8 @@ class MiniMaxAlfaBeta(object):
                 my_best_value = beta
                 if my_best_value < parent_alfa:
                     break
+
+            # self.chosen_move = valid_move
 
         return my_best_value
 
