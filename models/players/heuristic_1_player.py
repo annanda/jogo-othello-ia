66b81eac4e14c11e4b929145e13e6473f31805be
Annanda Sousa <annanda.sousa@gmail.com>
2015-12-23 21:28:05 -0200

    Concluída a heurística 1, em que se contam as peças no tabuleiro e escolhe-se a casa com o maior número de peças a serem ganhas.

--- models/players/heuristic_1_player.py
+++ models/players/heuristic_1_player.py
@@ -2,7 +2,20 @@
 import copy
 from models.move import Move
 from models.board import Board as board
+from models.minimax_alfabeta import MiniMaxAlfaBeta
 
+
+def heuristic_value(board, color):
+    """Recebe um tabuleiro e uma cor de jogador
+    Retorna a quantidade de peças que a cor especificada tem
+    """
+    heuristic = 0
+    for i in range(1, 9):
+        for j in range(1, 9):
+            if board.board[i][j] == color:
+                heuristic += 1
+
+    return heuristic
 
 class Heuristic1Player(object):
     """Implementa a herística que leva em conta a
@@ -11,23 +24,20 @@ class Heuristic1Player(object):
         self.color = color
 
     def play(self, board):
+        depth = 3
+        minimax = MiniMaxAlfaBeta(depth)
+        minimax.mini_max_alfa_beta(
+            board,
+            depth,
+            self.color,
+            float('-inf'),
+            float('inf'),
+            True,
+            heuristic_value
+        )
 
-        chosen_row = 1
-        chosen_colunm = 6
-        move = Move(chosen_row, chosen_colunm)
-        return move
+        return minimax.chosen_move
 
-    def heuristic_value(self, board, color):
-        """Recebe um tabuleiro e uma cor de jogador
-        Retorna a quantidade de peças que a cor especificada tem
-        """
-        heuristic = 0
-        for i in range(1, 9):
-            for j in range(1, 9):
-                if board.board[i][j] == color:
-                    heuristic += 1
-
-        return heuristic
 
     # def build_game_tree(self, board):
     #     valid_moves = board.valid_moves(self.color)
@@ -42,4 +52,4 @@ class Heuristic1Player(object):
     #             heuristic = heuristic_value
     #             chosen_move = move
 
-    # def chose_move(self):
+a_fix_for_globals_order = Heuristic1Player
