4c37f4ac9fabcc6330933292512bfe88f846d7ec
Annanda Sousa <annanda.sousa@gmail.com>
2016-01-04 02:41:06 -0200

    Adicionando nova heuristica

--- models/players/heuristic_4_player.py
+++ models/players/heuristic_4_player.py
@@ -3,29 +3,23 @@ from models.minimax_alfabeta import MiniMaxAlfaBeta
 
 
 def heuristic_value(board, color):
-    """Coin Parity
+    """Corners Captured
     Recebe um tabuleiro e uma cor de jogador
-    Retorna a quantidade de peças que a cor especificada tem
+    Retorna a quantidade de movimentos que a cor especificada tem
     """
     my_color = color
     enemy_color = board._opponent(my_color)
-    my_coins = 0
-    opponent_coins = 0
-    for i in range(1, 9):
-        for j in range(1, 9):
-            if board.board[i][j] == my_color:
-                my_coins += 1
-            if board.board[i][j] == enemy_color:
-                opponent_coins += 1
+    my_valid_moves = len(board.valid_moves(color))
+    enemy_valid_moves = len(board.valid_moves(enemy_color))
 
-    numerator = my_coins - opponent_coins
-    denominator = my_coins + opponent_coins
+    numerator = my_valid_moves - enemy_valid_moves
+    denominator = my_valid_moves + enemy_valid_moves
     heuristic = int(100 * numerator/denominator)
 
     return heuristic
 
 
-class Heuristic2Player(object):
+class Heuristic4Player(object):
     """Implementa a herística que leva em conta a
     quantidade de peças que cada um vai ter"""
     def __init__(self, color):
@@ -46,4 +40,4 @@ class Heuristic2Player(object):
 
         return minimax.chosen_move
 
-a_fix_for_globals_order = Heuristic2Player
+a_fix_for_globals_order = Heuristic4Player
