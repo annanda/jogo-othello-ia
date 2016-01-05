d0340d2da8c43df8e96bb1db0bb3506852dffb47
Annanda Sousa <annanda.sousa@gmail.com>
2016-01-05 07:17:02 -0200

    removendo print

--- models/players/best_player.py
+++ models/players/best_player.py
@@ -14,7 +14,6 @@ class BestPlayer(object):
         from models.minimax_alfabeta import MiniMaxAlfaBeta
         from models.players.heuristics import combined_heuristics_value
         from models.players.heuristics import get_gamephase
-        print ">>>phase: %d" % get_gamephase(board)
         depth = 4
         minimax = MiniMaxAlfaBeta(depth, )
         minimax.mini_max_alfa_beta(
